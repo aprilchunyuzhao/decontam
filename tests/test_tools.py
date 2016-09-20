974119a7247bfe08b9643bc7234e77348c1252e7
Chunyu Zhao <zhaoc1@email.chop.edu>
2016-09-20 09:49:30 -0400

    first

---
+++ tests/test_tools.py
@@ -0,0 +1,16 @@
+import unittest
+from io import StringIO
+
+from decontamlib.tools import (
+    _FilteringTool, None_human,
+    )
+
+
+class FilteringToolTests(unittest.TestCase):
+    def test_get_args(self):
+        self.assertEqual(_FilteringTool.get_argnames(), ["index"])
+        self.assertEqual(None_human.get_argnames(), [])
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests/test_utils.py
@@ -0,0 +1,47 @@
+import unittest
+import tempfile
+from io import StringIO
+
+from decontamlib import utils
+
+
+class TestIdExtract(unittest.TestCase):
+    def setUp(self):
+        self.fastq = tempfile.NamedTemporaryFile()
+        self.fastq.write("@1\nACG\n+\nEEE\n@2\nTTTT\n+\nEEEE\n@3\nANNN\n+\nEEEE\n".encode()) 
+        self.fastq.seek(0)
+
+    def test_ids(self):
+        ids = utils.parse_read_ids(self.fastq.name)
+        self.assertEqual(len(ids), 3)
+        self.assertTrue("1" in ids)
+        self.assertTrue("2" in ids)
+        self.assertTrue("3" in ids)
+
+
+class TestIdConsistency(unittest.TestCase):
+    def test_one_empty(self):
+        self.assertFalse(utils.check_all_read_ids_are_consistent(set(), {'a'}))
+
+    def test_different(self):
+        self.assertFalse(utils.check_all_read_ids_are_consistent({'a', 'b'}, {'a'}))
+        self.assertFalse(utils.check_all_read_ids_are_consistent({'a'}, {'a', 'b'}))
+
+    def test_the_same(self):
+        self.assertTrue(utils.check_all_read_ids_are_consistent({'a', 'b', 'c'}, {'c', 'a', 'b'}))
+
+
+class TestAddToolSample(unittest.TestCase):
+    def setUp(self):
+        self.human_annotation = [("read1", 1), ("read2", 1), ("read3", 0)]
+        self.wide_annotation = utils.add_tool_sample("tool", "sample", self.human_annotation)
+
+    def test_number_or_rows(self):
+        self.assertEqual(len(list(self.human_annotation)), len(list(self.wide_annotation)))
+
+    def test_number_or_columns(self):
+        self.assertEqual(len(list(self.human_annotation)[0]) + 2, len(list(self.wide_annotation)[0]))
+
+
+if __name__ == "__main__":
+    unittest.main()
