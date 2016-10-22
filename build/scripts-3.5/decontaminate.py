a1f33727e9914d169da67779093d5f339fc36179
Chunyu Zhao <zhaoc1@email.chop.edu>
2016-10-21 22:35:29 -0400

    change configs dir

---
+++ build/scripts-3.5/decontaminate.py
@@ -0,0 +1,3 @@
+#!/Users/zhaoc1/miniconda3/envs/pipeline1/bin/python
+from decontamlib.main import human_filter_main
+human_filter_main()
---
+++ build/scripts-3.5/make_index.py
@@ -0,0 +1,3 @@
+#!/Users/zhaoc1/miniconda3/envs/pipeline1/bin/python
+from decontamlib.main import make_index_main
+make_index_main()
