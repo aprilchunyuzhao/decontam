aa278decddc2fea4203e3fb462aff28e99366b91
Celeste Gaughan <gaughanc@Celestes-MacBook-Pro.local>
2017-08-23 09:56:50 -0400

    don't overwrite summary files

--- tests/benchmark.py
+++ tests/benchmark.py
@@ -4,10 +4,12 @@ from subprocess import call
 for p in map(lambda x: x/10.0,range(0,10)):
     for f in map(lambda x: x/10.0,range(0,10)):
         print("pct: " + str(p) + " frac: " + str(f))
-        call(["decontaminate.py", "--forward-reads", "data/humanseqs.fastq", "--organism", "human", "--pct", str(p), "--frac", str(f), "--output-dir", "../../fastq_files/output/", "--summary-file", "data/humanseqs.json"])
-        call(["decontaminate.py", "--forward-reads", "data/nonhumanseqs.fastq", "--organism", "human", "--pct", str(p), "--frac", str(f), "--output-dir", "../../fastq_files/output/", "--summary-file", "data/nonhumanseqs.json"])
+        hum_sum_file = "data/log/humanseqs" + "_" + str(p) + "_" + str(f) + ".json"
+        nonhum_sum_file = "data/log/nonhumanseqs" + "_" + str(p) + "_" + str(f) + ".json"
+        call(["decontaminate.py", "--forward-reads", "data/humanseqs.fastq", "--organism", "human", "--pct", str(p), "--frac", str(f), "--output-dir", "../../fastq_files/output/", "--summary-file", hum_sum_file])
+        call(["decontaminate.py", "--forward-reads", "data/nonhumanseqs.fastq", "--organism", "human", "--pct", str(p), "--frac", str(f), "--output-dir", "../../fastq_files/output/", "--summary-file", nonhum_sum_file])
         message = ["tru ", "fal ", "tru "]
-        for i, file in enumerate(["data/humanseqs.json", "data/nonhumanseqs.json"]):
+        for i, file in enumerate([hum_sum_file, nonhum_sum_file]):
             with open(file) as f:
                 res = json.loads(f.read())
                 mes = message[i] + "pos: "
