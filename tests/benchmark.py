5a47778abb6a71fd30a763861d136ee18d56fbe7
Celeste Gaughan <gaughanc@Celestes-MacBook-Pro.local>
2017-08-23 10:25:54 -0400

    add log and output to gitignore

--- tests/benchmark.py
+++ tests/benchmark.py
@@ -1,13 +1,17 @@
 import json
 from subprocess import call
+import os
+
+os.mkdir("data/log")
+os.mkdir("data/output")
 
 for p in map(lambda x: x/10.0,range(0,10)):
     for f in map(lambda x: x/10.0,range(0,10)):
         print("pct: " + str(p) + " frac: " + str(f))
         hum_sum_file = "data/log/humanseqs" + "_" + str(p) + "_" + str(f) + ".json"
         nonhum_sum_file = "data/log/nonhumanseqs" + "_" + str(p) + "_" + str(f) + ".json"
-        call(["decontaminate.py", "--forward-reads", "data/humanseqs.fastq", "--organism", "human", "--pct", str(p), "--frac", str(f), "--output-dir", "../../fastq_files/output/", "--summary-file", hum_sum_file])
-        call(["decontaminate.py", "--forward-reads", "data/nonhumanseqs.fastq", "--organism", "human", "--pct", str(p), "--frac", str(f), "--output-dir", "../../fastq_files/output/", "--summary-file", nonhum_sum_file])
+        call(["decontaminate.py", "--forward-reads", "data/humanseqs.fastq", "--organism", "human", "--pct", str(p), "--frac", str(f), "--output-dir", "data/output/", "--summary-file", hum_sum_file])
+        call(["decontaminate.py", "--forward-reads", "data/nonhumanseqs.fastq", "--organism", "human", "--pct", str(p), "--frac", str(f), "--output-dir", "data/output/", "--summary-file", nonhum_sum_file])
         message = ["tru ", "fal ", "tru "]
         for i, file in enumerate([hum_sum_file, nonhum_sum_file]):
             with open(file) as f:
