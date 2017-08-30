6727133e8301c88e778e5756451cd3cf31987ea2
Celeste Gaughan <gaughanc@carleton.edu>
2017-08-30 14:04:38 -0400

    remove build from github

--- build/scripts-3.5/decontaminate.py
+++
@@ -1,3 +0,0 @@
-#!/Users/gaughanc/miniconda3/envs/newpy35/bin/python
-from decontamlib.main import human_filter_main
-human_filter_main()
--- build/scripts-3.5/make_index.py
+++
@@ -1,3 +0,0 @@
-#!/Users/gaughanc/miniconda3/envs/newpy35/bin/python
-from decontamlib.main import make_index_main
-make_index_main()
