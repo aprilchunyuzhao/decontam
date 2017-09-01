889a9abdd70749f3094820cfd2986bbb5a213996
Celeste Gaughan <gaughanc@carleton.edu>
2017-09-01 10:29:48 -0400

    use abs paths

--- tests/benchmark.py
+++ tests/benchmark.py
@@ -1,13 +1,19 @@
 import json
 from subprocess import call
-import os
+import sys, os
+
+#define abs path
+path = os.path.abspath(os.path.dirname(sys.argv[0]))
+data_path = path + "/data"
 
 # create directories for summary files and output files if they don't exist
-if not os.path.exists("data/log"):
-    os.mkdir("data/log")
+log_path = data_path + "/log"
+if not os.path.exists(log_path):
+    os.mkdir(log_path)
 
-if not os.path.exists("data/output"):
-    os.mkdir("data/output")
+output_path = data_path + "/output"
+if not os.path.exists(output_path):
+    os.mkdir(output_path)
 
 # for pct between 0.0 and 1.0
 for p in map(lambda x: x/10.0,range(0,11)):
@@ -16,12 +22,12 @@ for p in map(lambda x: x/10.0,range(0,11)):
         # print pct and frac
         print("pct: " + str(p) + " frac: " + str(f))
         # name summary files
-        hum_sum_file = "data/log/humanseqs_{0}_{1}.json".format(p, f)
-        nonhum_sum_file = "data/log/nonhumanseqs_{0}_{1}.json".format(p, f)
+        hum_sum_file = "{}/humanseqs_{}_{}.json".format(log_path, p, f)
+        nonhum_sum_file = "{}/nonhumanseqs_{}_{}.json".format(log_path, p, f)
         # run decontam over human seqs
-        call(["decontaminate.py", "--forward-reads", "data/humanseqs.fastq", "--organism", "human", "--pct", str(p), "--frac", str(f), "--output-dir", "data/output/", "--summary-file", hum_sum_file])
+        call(["decontaminate.py", "--forward-reads", data_path + "/humanseqs.fastq", "--organism", "human", "--pct", str(p), "--frac", str(f), "--output-dir", output_path, "--summary-file", hum_sum_file])
         # run decontam over nonhuman seqs
-        call(["decontaminate.py", "--forward-reads", "data/nonhumanseqs.fastq", "--organism", "human", "--pct", str(p), "--frac", str(f), "--output-dir", "data/output/", "--summary-file", nonhum_sum_file])
+        call(["decontaminate.py", "--forward-reads", data_path + "/nonhumanseqs.fastq", "--organism", "human", "--pct", str(p), "--frac", str(f), "--output-dir", output_path, "--summary-file", nonhum_sum_file])
         # from human seqs summary file, read and print number of true positive ("true" in data) and false negatives ("false" in data)  
         # from nonhuman seqs summary file, read and print number of false positives ("true" in data) and true negatives ("false" in data)
         message = ["tru ", "fal ", "tru "]
